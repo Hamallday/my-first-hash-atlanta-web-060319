73bfa2bb845f2ab112e8a7f544aabb82311249fd
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-06-01 04:41:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,6 +20,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
+
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 shipping_manifest["oil paintings"]
 end
@@ -28,11 +29,11 @@ end
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   # add 2 muskets to the shipping_manifest hash below
-shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3, "muskets" => 2}
+shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-shipping_manifest.store("gunpowder" , 4)
+shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
